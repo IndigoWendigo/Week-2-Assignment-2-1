d7dd8e3a5c08ef189da086ce828e807e0057abf7
IndigoWendigo <123414451+IndigoWendigo@users.noreply.github.com>
2023-02-01 17:24:02 -0600

    Initial Commit

---
+++ Week2Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Week2Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = new int();
+            myVar = 9;
+
+            Console.WriteLine(-1 + 4 * myVar);
+            Console.WriteLine((35 + 5) % myVar);
+            Console.WriteLine(14 + -4 * 6 / 12);
+            Console.WriteLine(2 + 12 / 6 * 1 - myVar % 2);
+        }
+    }
+}
